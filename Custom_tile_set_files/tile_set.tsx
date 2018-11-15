9364d985bf11d633787282b112bf519c823944b9
BennettHaase-Divine-KU <42749983+BennettHaase-Divine-KU@users.noreply.github.com>
2018-11-15 01:14:42 -0600

    tile_set

---
+++ Custom_tile_set_files/tile_set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="tile_set" tilewidth="50" tileheight="30" tilecount="680" columns="20">
+ <image source="tile_set_1.png" width="1024" height="1024"/>
+</tileset>
