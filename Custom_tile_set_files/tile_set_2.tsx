48afd4038f58d7be6b71fcc56175d4235438eb75
BennettHaase-Divine-KU <42749983+BennettHaase-Divine-KU@users.noreply.github.com>
2018-11-15 01:14:50 -0600

    Update Custom_tile_set_files/tile_set_2.tsx

---
+++ Custom_tile_set_files/tile_set_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="tile_set_2" tilewidth="100" tileheight="60" tilecount="170" columns="10">
+ <image source="tile_set_1.png" width="1024" height="1024"/>
+</tileset>
