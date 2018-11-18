8284c1884707e6723e03711d85871ecaf1c943cb
Tsung Yu Ho <yuho850616@gmail.com>
2018-11-18 02:21:34 -0600

    basic timer

---
+++ IsoChai/Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	int countDownStartValue = 120; //limit time to two minutes
+	public Text timerUI;
+	void Start () {
+		countDownTimer();
+	}
+	void countDownTimer()
+	{
+		if(countDownStartValue>0)
+		{
+			timerUI.text = "Time left:"+countDownStartValue;
+			countDownStartValue--;
+			Invoke("countDownTimer",1.0f);
+		}
+		else
+		{
+			timerUI.text = "GameOver!";
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
