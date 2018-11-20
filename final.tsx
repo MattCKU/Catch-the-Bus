e8f3b462bb2f0279b8e385281c52fbc306ad5109
BennettHaase-Divine-KU <42749983+BennettHaase-Divine-KU@users.noreply.github.com>
2018-11-19 20:28:21 -0600

    Implemented new Tile set
    Wow!

---
+++ final.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="final" tilewidth="100" tileheight="60" tilecount="340" columns="10">
+ <image source="IsoChai/Assets/ISO/Spritesheet/landscapeTiles_sheet.png" width="1024" height="2048"/>
+</tileset>
