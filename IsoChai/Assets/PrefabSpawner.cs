4be1f8cf04f10595347487a8e86a35630c9864d3
BennettHaase-Divine-KU <42749983+BennettHaase-Divine-KU@users.noreply.github.com>
2018-11-23 11:47:29 -0600

    Added spawner in spawn_test scene

---
+++ IsoChai/Assets/PrefabSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour {
+
+	private float nextSpawnTime;
+
+	[SerializeField]
+	private GameObject spawnObject;
+	[SerializeField]
+	private float spawnDelay=10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(shouldSpawn()){
+			Spawn();
+		}
+	}
+	private void Spawn(){
+		nextSpawnTime = Time.time + spawnDelay;
+		Instantiate(spawnObject, transform.position, transform.rotation);
+	}
+	private bool shouldSpawn(){
+		return Time.time >= nextSpawnTime;
+	}
+
+}
+
