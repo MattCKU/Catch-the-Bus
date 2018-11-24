f92279811e541508a5fdfb0299dd9978ef615a0a
BennettHaase-Divine-KU <42749983+BennettHaase-Divine-KU@users.noreply.github.com>
2018-11-24 02:23:21 -0600

    Add cat movement

---
+++ IsoChai/Assets/movement_cat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement_cat : MonoBehaviour {
+
+	Rigidbody rb;
+
+	[SerializeField] 
+	private Vector3 movementDir = new Vector3(-60,0,-30);
+	[SerializeField] 
+	private float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		transform.eulerAngles = new Vector3(90,320,0);
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.MovePosition(transform.position + movementDir.normalized*speed*Time.deltaTime);
+		//transform.Translate(movementDir.normalized*speed*Time.deltaTime);
+		
+		//transform.Rotate(0,240,0);
+	}
+}
