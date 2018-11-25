7142d6ace7e0ec99ae84bbd4b3052e9a6f9c6d6d
bradandrews510 <42749434+bradandrews510@users.noreply.github.com>
2018-11-24 21:06:15 -0600

    First Test Case
    Added a way to test.

---
+++ IsoChai/Assets/Editor/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    public Movement Movement;
+    
+	private void Start ()
+    {
+        Movement = new Movement(Speed);
+	}
+	
+	private void Update ()
+    {
+        transform.position += Movement.Calculate(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Time.deltaTime);
+	}
+}
