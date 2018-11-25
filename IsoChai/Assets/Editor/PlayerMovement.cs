bc9ffb353994a05b715e508690a1b201136bdd1a
bradandrews510 <42749434+bradandrews510@users.noreply.github.com>
2018-11-24 21:33:34 -0600

    Test Improvments
    More work done on Test case for player movement.

--- IsoChai/Assets/Editor/PlayerMovement.cs
+++ IsoChai/Assets/Editor/PlayerMovement.cs
@@ -6,14 +6,49 @@ public class PlayerMovement : MonoBehaviour
 {
     public float Speed;
     public Movement Movement;
-    
-	private void Start ()
+
+    void Start()
     {
         Movement = new Movement(Speed);
-	}
-	
-	private void Update ()
+    }
+
+    void Update()
     {
         transform.position += Movement.Calculate(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Time.deltaTime);
-	}
+    }
+}
+
+public class Movement
+{
+    public float Speed;
+    public Movement(float speed)
+    {
+        Speed = speed;
+    }
+
+    public Vector3 Calculate(float h, float v, float deltaTime)
+    {
+        var x = h * Speed * deltaTime;
+        var z = v * Speed * deltaTime;
+
+        return new Vector3(x, 0, z);
+    }
+}
+
+using NUnity.Framework;
+namespace Tests.Editor
+{
+    public class MovementTests
+    {
+        [Test]
+        public void Moves_Along_X()
+        {
+            Assert.AreEqual(1, new Movement(1).Calculate(1, 0, 1).x, 0.1f);
+        }
+        [Test]
+        public void Moves_Along_Z()
+        {
+            Assert.AreEqual(1, new Movement(1).Calculate(1, 0, 1).z, 0.1f);
+        }
+    }
 }
