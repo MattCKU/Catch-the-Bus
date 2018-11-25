11eb0847b59517a01fe623d076ee52bcdf791bce
mattcKU <mattcherryku@gmail.com>
2018-11-25 11:29:56 -0600

    Added event trigger script for win case.
    Added BusCollison.cs that triggers win scene if player collides with it.

---
+++ IsoChai/Assets/BusCollide.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BusCollide : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Entered.");
+		SceneManager.LoadScene("finalwin");
+		//player.transform.position = new Vector3(0, y: 2, z: 0	);
+	}
+}
