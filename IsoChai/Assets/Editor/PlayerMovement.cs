2692908f194eaf2de10c2410a4e8e805df0b1fe7
Tsung Yu Ho <yuho850616@gmail.com>
2018-11-24 20:16:25 -0800

    comment out movement

--- IsoChai/Assets/Editor/PlayerMovement.cs
+++ IsoChai/Assets/Editor/PlayerMovement.cs
@@ -1,54 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerMovement : MonoBehaviour
-{
-    public float Speed;
-    public Movement Movement;
-
-    void Start()
-    {
-        Movement = new Movement(Speed);
-    }
-
-    void Update()
-    {
-        transform.position += Movement.Calculate(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Time.deltaTime);
-    }
-}
-
-public class Movement
-{
-    public float Speed;
-    public Movement(float speed)
-    {
-        Speed = speed;
-    }
-
-    public Vector3 Calculate(float h, float v, float deltaTime)
-    {
-        var x = h * Speed * deltaTime;
-        var z = v * Speed * deltaTime;
-
-        return new Vector3(x, 0, z);
-    }
-}
-
-using NUnity.Framework;
-namespace Tests.Editor
-{
-    public class MovementTests
-    {
-        [Test]
-        public void Moves_Along_X()
-        {
-            Assert.AreEqual(1, new Movement(1).Calculate(1, 0, 1).x, 0.1f);
-        }
-        [Test]
-        public void Moves_Along_Z()
-        {
-            Assert.AreEqual(1, new Movement(1).Calculate(1, 0, 1).z, 0.1f);
-        }
-    }
-}
+// ﻿using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+//
+// public class PlayerMovement : MonoBehaviour
+// {
+//     public float Speed;
+//     public Movement Movement;
+//
+//     void Start()
+//     {
+//         Movement = new Movement(Speed);
+//     }
+//
+//     void Update()
+//     {
+//         transform.position += Movement.Calculate(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Time.deltaTime);
+//     }
+// }
+//
+// public class Movement
+// {
+//     public float Speed;
+//     public Movement(float speed)
+//     {
+//         Speed = speed;
+//     }
+//
+//     public Vector3 Calculate(float h, float v, float deltaTime)
+//     {
+//         var x = h * Speed * deltaTime;
+//         var z = v * Speed * deltaTime;
+//
+//         return new Vector3(x, 0, z);
+//     }
+// }
+//
+// using NUnity.Framework;
+// namespace Tests.Editor
+// {
+//     public class MovementTests
+//     {
+//         [Test]
+//         public void Moves_Along_X()
+//         {
+//             Assert.AreEqual(1, new Movement(1).Calculate(1, 0, 1).x, 0.1f);
+//         }
+//         [Test]
+//         public void Moves_Along_Z()
+//         {
+//             Assert.AreEqual(1, new Movement(1).Calculate(1, 0, 1).z, 0.1f);
+//         }
+//     }
+// }
