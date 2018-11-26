f50d9371fe74be5722105aa305ed34faf11b9a3f
mattcKU <mattcherryku@gmail.com>
2018-11-25 22:15:12 -0600

    Added Beginning of Test

--- IsoChai/Assets/MainMenu.cs
+++ IsoChai/Assets/MainMenu.cs
@@ -23,4 +23,9 @@ public class MainMenu : MonoBehaviour {
 	{
 		SceneManager.LoadScene("introduction");
 	}
+
+	public void test()
+	{
+		SceneManager.LoadScene("test");
+	}
 }
---
+++ IsoChai/Assets/Playmode Tests/TestScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestScript {
+
+    [Test]
+    public void TestScriptSimplePasses() {
+        // Use the Assert class to test conditions.
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator TestScriptWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+    }
+}
