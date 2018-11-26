4c2ef93702dd26db312a69b9b38d5c1564f5a346
mattcKU <mattcherryku@gmail.com>
2018-11-25 19:53:41 -0600

    Added Top Sidewalk Slow Zone Object

---
+++ IsoChai/Assets/Slow_Zone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slow_Zone : MonoBehaviour {
+
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("On top sidewalk. Slowing Down.");
+
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		Debug.Log("Leaving the top sidewalk. Speeding up.");
+	}
+}
